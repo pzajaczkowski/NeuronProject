9ba6891e4b762af6b1155af885c5d3e775224d56
matmak13 <maksimowiczmateusz13@gmail.com>
2022-06-07 12:59:25 +0200

    Neuron classes

---
+++ Neuron.cs
@@ -0,0 +1,23 @@
+﻿namespace NeuronProject;
+
+public abstract class Neuron
+{
+    public IList<decimal> Weights { get; init; } = new List<decimal>();
+    public IList<decimal> Inputs { get; init; } = new List<decimal>();
+    public decimal ExpectedOutput { get; set; }
+    protected abstract decimal ActivationFunction(decimal x);
+
+    public virtual decimal Calculate()
+    {
+        if (Weights.Count != Inputs.Count || !Weights.Any())
+            throw new Exception("Weights.Count() != Inputs.Count() || !Weights.Any()");
+
+        decimal sum = 0;
+
+        for (var i = 0; i < Weights.Count; i++) sum += Weights[i] * Inputs[i];
+
+        return ActivationFunction(sum);
+    }
+
+    public abstract void Learn();
+}
