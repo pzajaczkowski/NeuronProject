fc1aeb2320b9efcce3ba3581d0b82e99c138f477
matmak13 <maksimowiczmateusz13@gmail.com>
2022-06-07 13:22:14 +0200

    bias

--- Neuron.cs
+++ Neuron.cs
@@ -1,12 +1,18 @@
 ﻿namespace NeuronProject;
 
-public abstract class Neuron
+internal abstract class Neuron
 {
     public IList<decimal> Weights { get; init; } = new List<decimal>();
-    public IList<decimal> Inputs { get; init; } = new List<decimal>();
+    public IList<decimal> Inputs { get; set; } = new List<decimal>();
+    public decimal Bias { get; init; }
     public decimal ExpectedOutput { get; set; }
     protected abstract decimal ActivationFunction(decimal x);
 
+    /// <summary>
+    ///     Oblicza wyjście neuronu
+    /// </summary>
+    /// <returns> Wyjście neuronu </returns>
+    /// <exception cref="Exception"></exception>
     public virtual decimal Calculate()
     {
         if (Weights.Count != Inputs.Count || !Weights.Any())
@@ -16,7 +22,7 @@ public abstract class Neuron
 
         for (var i = 0; i < Weights.Count; i++) sum += Weights[i] * Inputs[i];
 
-        return ActivationFunction(sum);
+        return ActivationFunction(sum + Bias);
     }
 
     public abstract void Learn();
