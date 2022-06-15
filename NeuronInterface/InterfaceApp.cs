2227d0fbbe69547f140f63834784e5746d6b8525
matmak13 <maksimowiczmateusz13@gmail.com>
2022-06-15 16:45:07 +0200

    duzo rzeczy istotny commit

--- NeuronInterface/InterfaceApp.cs
+++ NeuronInterface/InterfaceApp.cs
@@ -1,4 +1,6 @@
-﻿using NeuronProject;
+﻿using System;
+using System.Collections.Generic;
+using NeuronProject;
 
 namespace NeuronInterface;
 
@@ -10,7 +12,62 @@ internal class InterfaceApp
         Iterations
     }
 
-    public MODE Mode { get; set; } = MODE.Error;
+    public enum NEURON
+    {
+        Perceptron,
+        Adaline
+    }
 
-    public NeuronApp NeuronApp { get; private set; }
+    public enum STATE
+    {
+        Waiting,
+        Stopped,
+        Running
+    }
+
+    private MODE _mode = MODE.Error;
+    private NEURON _neuron = NEURON.Perceptron;
+
+    public STATE State { get; } = STATE.Waiting;
+
+    public MODE Mode
+    {
+        get => _mode;
+        set
+        {
+            if (State == STATE.Running)
+                throw new Exception("Nie można zmienić trybu w czasie działania aplikacji");
+
+            _mode = value;
+        }
+    }
+
+    public NEURON Neuron
+    {
+        get => _neuron;
+        set
+        {
+            if (State == STATE.Running)
+                throw new Exception("Nie można zmienić neuronu w czasie działania aplikacji");
+
+            _neuron = value;
+
+            NeuronApp.Neuron = value switch
+            {
+                NEURON.Perceptron => new PerceptronNeuron(),
+                NEURON.Adaline => new AdalineNeuron(),
+                _ => throw new Exception("how?????")
+            };
+        }
+    }
+
+    private NeuronApp NeuronApp { get; } = new() { Neuron = new PerceptronNeuron() };
+
+    public void LoadDataFromDataList(List<Data> data)
+    {
+        if (State != STATE.Waiting)
+            throw new Exception("Nie można wczytać danych w czasie działania aplikacji"); // xd
+
+        NeuronApp.LoadDataFromDataList(data);
+    }
 }
